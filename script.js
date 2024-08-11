e8910035bc45b40a9e39adeadd551f871a08e54c
Neelam <1neelamrawat@gmail.com>
2024-08-11 14:46:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlide(){
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block'
+}
+changeSlide();
+
+
+prev_btn.addEventListener('click', (e)=>{
+    if(n>0){
+        n--;
+    }else{
+        n=imgs.length - 1;
+    }
+    changeSlide();
+})
+
+next_btn.addEventListener('click', (e)=>{
+    if(n < imgs.length -1){
+        n++;
+    }else{
+        n=0;
+    }
+    changeSlide();
+})
+
+const scrollContainer = document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    })
+    
+}
+
+const dropdownbtn = document.querySelectorAll(".dropdownbtn")
+const dropdownbox = document.querySelectorAll(".dropdownbox")
+
+
+dropdownbtn.forEach((dropb,index)=>{
+    dropb.addEventListener("mouseover",()=>{
+
+        dropdownbox[index].classList.remove("active")
+    })
+    dropb.addEventListener("mouseout",()=>{
+
+        dropdownbox[index].classList.add("active")
+    })
+})
